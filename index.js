87ba6da3c06e66629258ad0df39a6730e525f2bb
Michael Wuergler <wuergler@gmail.com>
2017-02-13 16:31:32 +0000

    refactor

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = function (input) {
     }
 
     var date = new Date(input);
+    var dateString = date.toString();
     var validDate;
 
     // Return false on Number values
@@ -13,18 +14,26 @@ module.exports = function (input) {
       return false;
     };
 
+    if (dateString === "Invalid Date") {
+      return false;
+    }
+
     // JavaScript Quirk: if the returned Date string
     // has two strings in it, then you likely gave the
     // Date constructor a strange value like new Date('foo 100')
     // which will result in a weird date output (notice the two spaces):
     // Fri Jan 01  100 00:00:00 GMT+0000 (WET)
-    var hasTwoSpaces = date.toString().search('  ') > -1;
+    var hasTwoSpaces = dateString.search('  ') > -1;
+
+    if (hasTwoSpaces) {
+      return false;
+    }
 
     try {
-      validDate = !isNaN(date.getTime());
+      validDate = !isNaN(date.getTime()) && typeof date.getTime() === 'number';
     } catch (err) {
       validDate = false;
     }
 
-    return date.toString() !== "Invalid Date" && !hasTwoSpaces && validDate;
+    return validDate;
 };
